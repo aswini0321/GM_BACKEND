cd56833dcd6c7d127b644235635b9a27905dd09d
aswini0321 <pedadaaswini2004@gmail.com>
2025-05-01 12:44:50 +0530

    change script

--- models/Question.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const questionSchema = new mongoose.Schema({
-  question: String,
-  options: [String]
-});
-
-const QuestionModel = mongoose.model("Question", questionSchema);
-module.exports = QuestionModel;
